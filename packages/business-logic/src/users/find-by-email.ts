592a92bff91faad28ca0435cf674aa7cb147e952
Bitter <yunsde18@gmail.com>
2025-10-27 08:34:29 -0500

    chore: new information

--- packages/business-logic/src/users/find-by-email.ts
+++ packages/business-logic/src/users/find-by-email.ts
@@ -2,11 +2,11 @@ import { Collection, getModel } from "@mdi/constant-definitions";
 import type { User } from "@mdi/contracts";
 import { UserSchemaMongo } from "@mdi/contracts";
 
-export const findByEmail = async (email: string): Promise<User> => {
+export const findByEmail = async (email: string): Promise<User | null> => {
     const model = getModel<User>(Collection.USERS, UserSchemaMongo);
 
     const user = await model.findOne({ email });
-    if (!user) throw new Error("User not found");
+    if (!user) return null;
 
     return user;
 };
